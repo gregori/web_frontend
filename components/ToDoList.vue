9242c05a8d64939a268e45ea1de5dd0cf70971d9
Rodrigo Gregori <rodrigo.gregori@gmail.com>
2022-02-20 10:18:38 -0300

    Primeiras alterações

---
+++ components/ToDoList.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="to-do-container">
+    <div class="create-container">
+      <input v-model="titleOfNewToDo" type="text">
+      <button @click="create">
+        Add
+      </button>
+    </div>
+    <div class="to-do-list-container" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ToDoList',
+  props: {
+    todos: {
+      type: Array,
+      default () {
+        return []
+      }
+    }
+  },
+  data () {
+    return {
+      titleOfNewToDo: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.to-do-container {
+  width: 400px;
+  margin: 50px auto auto;
+  display: block;
+
+  .create-container {
+    width: 100%;
+    height: 50px;
+    box-shadow: 0 10px 20px rgba(0, 0, 0, 0.1);
+
+    input {
+      height: 50px;
+      width: 250px;
+      border: none;
+      padding-left: 10px;
+      font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+        Roboto, 'Helvetica Neue', Arial, sans-serif;
+      font-size: 18px;
+      &:focus {
+        outline: none;
+      }
+    }
+
+    button {
+      height: 45px;
+      float: right;
+      background: none;
+      width: auto;
+      margin: 0;
+      border: none;
+      cursor: pointer;
+      text-align: right;
+      padding-right: 20px;
+      font-size: 18px;
+      &:focus {
+        outline: none;
+      }
+    }
+  }
+}
+
+.to-do-list-container {
+  width: 400px;
+  margin: 50px auto auto;
+  display: block;
+  box-shadow: 0 10px 20px rgba(0, 0, 0, 0.1);
+}
+
+</style>
---
+++ pages/todo.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+    <h1 class="title">
+      My To Do List
+    </h1>
+    <to-do-list :todos="todos" />
+  </div>
+</template>
+
+<script>
+import ToDoList from '~/components/ToDoList'
+export default {
+  name: 'Todo',
+  components: {
+    ToDoList
+  },
+  asyncData (ctx) {
+    return {
+      todos: [
+        {
+          id: '1',
+          title: 'A',
+          completed: false
+        },
+        {
+          id: '2',
+          title: 'B',
+          completed: true
+        }
+      ]
+    }
+  },
+  data () {
+    return {
+      todos: {
+        type: Array
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
